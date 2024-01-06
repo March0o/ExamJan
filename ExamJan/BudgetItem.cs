fe865dff0b82c2f6f5f86113b8dad13d3aa72d83
Martynas Cepokaitis <S00234148@atu.ie>
2024-01-06 10:20:03 +0000

    Corrected enum

--- ExamJan/BudgetItem.cs
+++ ExamJan/BudgetItem.cs
@@ -8,19 +8,22 @@ namespace ExamJan
 {
     internal class BudgetItem : IComparable
     {
+        public enum BudgetItemType { Income, Expense }
+
         // Properties
         public string Name { get; set; }
         public decimal Amount { get; set; }
-        public enum ItemType { Income, Expense}
+        public BudgetItemType ItemType {  get; set; } 
         public DateTime Date { get; set; }
         public bool Recurring { get; set; }
 
         //  Constructors
         public BudgetItem() { }
-        public BudgetItem(string name, decimal amount, DateTime date, bool recurring)
+        public BudgetItem(string name, decimal amount,BudgetItemType itemtype, DateTime date, bool recurring)
         {
             Name = name;
             Amount = amount;
+            ItemType = itemtype;
             Date = date;
             Recurring = recurring;
         }
