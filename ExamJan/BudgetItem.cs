34ff6090c384f28fb759ac02c67cdc29c093307d
Martynas Cepokaitis <S00234148@atu.ie>
2024-01-06 09:48:15 +0000

    Created Class

---
+++ ExamJan/BudgetItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamJan
+{
+    internal class BudgetItem
+    {
+        // Properties
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public enum ItemType { Income, Expense}
+        public DateTime Date { get; set; }
+        public bool Recurring { get; set; }
+
+        //  Constructors
+        public BudgetItem() { }
+        public BudgetItem(string name, decimal amount, Enum itemType, DateTime date, bool recurring)
+        {
+            Name = name;
+            Amount = amount;
+            Date = date;
+            Recurring = recurring;
+        }
+
+        //  Methods
+        public override string ToString()
+        {
+            return $"{Date.ToString("dddd")} {Name} {Amount} - {Recurring}";
+        }
+    } // End of Class
+}
